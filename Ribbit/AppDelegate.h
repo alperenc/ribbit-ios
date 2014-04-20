8aa2d84529d142d104cfb7cfaa534d2b72c9075b
Alp Eren Can <alperenc@gmail.com>
2014-04-20 16:04:54 +0300

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Alp Eren Can on 20/04/14.
+//  Copyright (c) 2014 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Alp Eren Can on 20/04/14.
+//  Copyright (c) 2014 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
